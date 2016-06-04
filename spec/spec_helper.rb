a8e18c58bd33d8269a35e820178821259958c7d9
Bryce Kerley <bkerley@brycekerley.net>
2016-06-04 13:16:38 -0400

    use vcr to save on http dependencies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'miami_dade_geo'
+
+require 'vcr'
+
+VCR.configure do |vcr|
+  vcr.cassette_library_dir = 'spec/cassettes'
+  vcr.hook_into :webmock
+  vcr.configure_rspec_metadata!
+end
