70d6a4fec4710a125cf3e228691535e19efb3bcb
Bryce Kerley <bkerley@brycekerley.net>
2016-06-04 15:33:41 -0400

    implement more municipality stuff

--- lib/miami_dade_geo/geo_attribute_client.rb
+++ lib/miami_dade_geo/geo_attribute_client.rb
@@ -6,6 +6,29 @@ module MiamiDadeGeo
     include Singleton
 
     WSDL_URL = "http://gisws.miamidade.gov/gisdata/GeoAttribute.asmx?wsdl"
+
+    def all_fields(table, field_name, value)
+      body = savon.
+             call(:get_all_fields_records_given_a_field_name_and_value,
+                  message: {
+                    'strFeatureClassOrTableName' => table,
+                    'strFieldNameToSearchOn' => field_name,
+                    'strValueOfFieldToSearchOn' => value
+                  }).
+             body
+
+      resp = body[:get_all_fields_records_given_a_field_name_and_value_response]
+      rslt = resp[:get_all_fields_records_given_a_field_name_and_value_result]
+      polys = rslt[:diffgram][:document_element][:municipality_poly]
+
+      poly = if polys.is_a? Array
+               polys.first
+             elsif polys.is_a? Hash
+               polys
+             else
+               fail "Unexpected polys #{polys.class.name}, wanted Array or Hash"
+             end
+    end
     
     def savon
       @savon ||= Savon.client(wsdl: WSDL_URL)
--- lib/miami_dade_geo/municipality.rb
+++ lib/miami_dade_geo/municipality.rb
@@ -1,40 +1,43 @@
 require 'miami_dade_geo/geo_attribute_client'
+require 'miami_dade_geo/get_closest_feature_client'
 
 module MiamiDadeGeo
   class Municipality
-    attr_reader :name
+    attr_reader :name, :munic_code
+
+    def self.new_with_name(name)
+      canonicalized_name = name.upcase
+
+      poly = GeoAttributeClient.instance.
+             all_fields('municipality_poly',
+                        'name',
+                        canonicalized_name)
+
+      new poly
+    end
 
     def self.new_with_code(munic_code)
       canonicalized_munic_code = "%02d" % [munic_code.to_i]
 
-      body = GeoAttributeClient.instance.savon.
-             call(:get_all_fields_records_given_a_field_name_and_value,
-                  message: {
-                    'strFeatureClassOrTableName' => 'municipality_poly',
-                    'strFieldNameToSearchOn' => 'municid',
-                    'strValueOfFieldToSearchOn' => canonicalized_munic_code
-                  })
-             .body
+      poly = GeoAttributeClient.instance.
+             all_fields('municipality_poly',
+                        'municid',
+                        canonicalized_munic_code)
 
-      resp = body[:get_all_fields_records_given_a_field_name_and_value_response]
-      rslt = resp[:get_all_fields_records_given_a_field_name_and_value_result]
-      polys = rslt[:diffgram][:document_element][:municipality_poly]
+      new poly
+    end
 
-      poly = if polys.is_a? Array
-               polys.first
-             elsif polys.is_a? Hash
-               polys
-             else
-               fail "Unexpected polys #{polys.class.name}, wanted Array or Hash"
-             end
-
-      new poly[:name]
+    def self.new_with_xy(xy_hash)
+      p GetClosestFeatureClient.instance.savon.operations
+      body = GetClosestFeatureClient.instance.savon.
+             call(:get_closest_feature_from_x_y_all_attribs)
     end
 
     private
 
-    def initialize(name)
-      @name = name
+    def initialize(poly)
+      @name = poly[:name]
+      @munic_code = poly[:municid]
     end
   end
 end
