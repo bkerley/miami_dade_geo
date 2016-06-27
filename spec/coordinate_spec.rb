d5758576bb3d1c29c0a17e75754a8f58f301bba0
Bryce Kerley <bkerley@brycekerley.net>
2016-06-27 19:37:52 -0400

    xy latlong conversion test

---
+++ spec/coordinate_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'miami_dade_geo/coordinate'
+
+describe MiamiDadeGeo::Coordinate, :vcr do
+
+  let(:panther_xy){ { x: 919623.87509118, y: 533780.31245147 } }
+  let(:panther_latlong){ { long: -80.199245566240819, lat: 25.7999410593211 } }
+
+  it 'turns xy coordinates into latlong' do
+    coord = described_class.new panther_xy
+    expect(coord.lat).to be_within(0.001).of(panther_latlong[:lat])
+    expect(coord.long).to be_within(0.001).of(panther_latlong[:long])
+  end
+
+  it 'turns latlong coordinates into xy' do
+    coord = described_class.new panther_latlong
+    expect(coord.x).to be_within(0.001).of(panther_xy[:x])
+    expect(coord.y).to be_within(0.001).of(panther_xy[:y])
+  end
+end
