82d527319a95d320685ed98d641ee9081ee4d514
Bryce Kerley <bkerley@brycekerley.net>
2016-07-11 19:45:51 -0400

    find radiuses now work smarter

--- lib/miami_dade_geo/get_closest_feature_client.rb
+++ lib/miami_dade_geo/get_closest_feature_client.rb
@@ -7,17 +7,15 @@ module MiamiDadeGeo
 
     WSDL_URL = "http://gisws.miamidade.gov/gisxyservices/GetClosestFeature.asmx?wsdl"
 
-    FIND_RADIUSES = [0, 100, 1000, 5280, (20 * 5280)]
+    FIND_RADIUSES = [0, 10, 100, 1000, 5280, (20 * 5280)]
 
     def find_feature(xy_hash, feature_class)
       feature = nil
       FIND_RADIUSES.each do |radius|
-        p xy_hash
         found = get_closest_feature(feature_class,
                                     xy_hash[:x].to_s,
                                     xy_hash[:y].to_s,
                                     radius.to_s)
-        p found
       end
     end
 
@@ -32,11 +30,13 @@ module MiamiDadeGeo
                   }).
              body
 
-      p body
-
-      resp = body[:get_closest_feature_from_xy_all_atrbts_response]
-      rslt = resp[:get_closest_feature_from_xy_all_atrbts_result]
-      poly = rslt[:diffgram][:document_element]
+      begin
+        resp = body[:get_closest_feature_from_xy_all_atrbts_response]
+        rslt = resp[:get_closest_feature_from_xy_all_atrbts_result]
+        poly = rslt[:diffgram][:document_element][:results]
+      rescue
+        nil
+      end
     end
 
     def savon
